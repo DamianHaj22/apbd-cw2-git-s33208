d63e5b67e7bf57a816a6e0cc25521a955cdf3ef4
Damian Hajduk <s33208@pjwstk.edu.pl>
2026-03-25 20:06:08 +0100

    Utworzenie klasy IPenaltyCalculator

---
+++ EquipmentRental/EquipmentRental/Application/Policies/IPenaltyCalculator.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EquipmentRental.Application.Policies
+{
+    public interface IPenaltyCalculator
+    {
+        decimal CalculatePenalty(DateTime expectedReturnDate, DateTime actualReturnDate);
+    }
+}
