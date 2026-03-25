0dd9563f9173fd12dddd44b5398fe7dae537e616
Damian Hajduk <s33208@pjwstk.edu.pl>
2026-03-25 19:12:26 +0100

    Dodanie klasy Projector w folderze Domain/Models

---
+++ EquipmentRental/EquipmentRental/Domain/Models/Projector.cs
@@ -0,0 +1,15 @@
+﻿namespace EquipmentRental.Domain.Models
+{
+    public class Projector : Equipment
+    {
+        public int NativeResolutionWidth { get; }
+        public int Lumens { get; }
+
+        public Projector(string name, string serialNumber, int nativeResolutionWidth, int lumens) : base(name,
+            serialNumber)
+        {
+            NativeResolutionWidth = nativeResolutionWidth;
+            Lumens = lumens;
+        }
+    }
+}
