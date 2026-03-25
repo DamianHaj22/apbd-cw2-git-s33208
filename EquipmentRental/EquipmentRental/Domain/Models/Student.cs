3a0c2d9eff8e30ccfa011e6f8b78c8c8728f8d93
Damian Hajduk <s33208@pjwstk.edu.pl>
2026-03-25 19:27:00 +0100

    Utworzenie klasy Student folderze Domain.Models

---
+++ EquipmentRental/EquipmentRental/Domain/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace EquipmentRental.Domain.Models
+{
+    public class Student : User
+    {
+        public string StudentName { get; }
+        
+        public Student(string firstName, string lastName, string email, string phoneNumber) : base(firstName, lastName, UserType.Student)
+            {
+            StudentName = firstName;
+            }
+    }
+}
