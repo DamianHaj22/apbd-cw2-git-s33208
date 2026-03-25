337ffe429aa207c7487bdbff8afc5cf166aa47b0
Damian Hajduk <s33208@pjwstk.edu.pl>
2026-03-25 19:29:44 +0100

    Utworzenie klasy Employee folderze Domain.Models

---
+++ EquipmentRental/EquipmentRental/Domain/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace EquipmentRental.Domain.Models
+{
+    public class Employee : User
+    {
+        public string Department { get; }
+
+        public Employee(string firstName, string lastName, string department) : base(firstName, lastName,
+            UserType.Employee)
+        {
+            Department = department;
+        }
+    }
+}
