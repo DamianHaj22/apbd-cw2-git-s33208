1e29e65900b028f7f89577d32056be79e3ccba3e
Damian Hajduk <s33208@pjwstk.edu.pl>
2026-03-25 19:56:43 +0100

    Utworzenie klasy IRentalLimitPolicy

---
+++ EquipmentRental/EquipmentRental/Application/Policies/IRentalLimitPolicy.cs
@@ -0,0 +1,9 @@
+﻿using EquipmentRental.Domain.Models;
+
+namespace EquipmentRental.Application.Policies
+{
+    public interface IRentalLimitPolicy
+    {
+        int GetMaxActiveRentals(UserType userType);
+    }
+}
