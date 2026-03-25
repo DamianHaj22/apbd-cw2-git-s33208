bf3892def8a04a57e06325bdc2d267967209e7d1
Damian Hajduk <s33208@pjwstk.edu.pl>
2026-03-25 19:15:33 +0100

    Utworzenie klasy Camera w folderze Domain/Models

---
+++ EquipmentRental/EquipmentRental/Domain/Models/Camera.cs
@@ -0,0 +1,15 @@
+﻿namespace EquipmentRental.Domain.Models
+{
+    public class Camera : Equipment
+    {
+        public bool HasInterchangeableLens { get; }
+        public double MegaPixels { get; }
+
+        public Camera(string name, string serialNumber, bool hasInterchangeableLens, double megaPixels) : base(name,
+            serialNumber)
+        {
+            HasInterchangeableLens = hasInterchangeableLens;
+            MegaPixels = megaPixels;
+        }
+    }
+}
