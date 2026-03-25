b9828a50fce980efd6f3459f4e0d688ae224b9d4
Damian Hajduk <s33208@pjwstk.edu.pl>
2026-03-25 19:10:29 +0100

    Dodanie klasy Laptop w folderze Domain/Models

---
+++ EquipmentRental/EquipmentRental/Domain/Models/Laptop.cs
@@ -0,0 +1,14 @@
+﻿namespace EquipmentRental.Domain.Models
+{
+    public class Laptop : Equipment //dziedziczenie
+    {
+        public int RamSizeGB { get; }
+        public string ProcessorModel { get; }
+
+        public Laptop(string name, string serialNumber, int ramSizeGB, string processorModel) : base(name, serialNumber)
+        {
+            RamSizeGB = ramSizeGB;
+            ProcessorModel = processorModel;
+        }
+    }
+}
