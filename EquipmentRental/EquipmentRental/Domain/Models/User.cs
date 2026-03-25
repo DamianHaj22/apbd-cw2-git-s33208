f36bc8b6ee4acc2e3f030f7c464c3b82a1149a64
Damian Hajduk <s33208@pjwstk.edu.pl>
2026-03-25 19:23:42 +0100

    Utworzenie klasy bazowej User w folderze Domain.Models

---
+++ EquipmentRental/EquipmentRental/Domain/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EquipmentRental.Domain.Models
+{
+    public abstract class User
+    {
+        public Guid Id { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public UserType Type { get; }
+
+        protected User(string firstName, string lastName, UserType type)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            Type = type;
+        }
+    }
+}
